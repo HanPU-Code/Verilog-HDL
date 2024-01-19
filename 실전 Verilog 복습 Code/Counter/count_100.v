29ed4dc934aeefef6cff5ae1d9096ad2e9ece7ec
Hanyul <croc0001@naver.com>
2024-01-19 14:50:28 +0900

    2회차 (2) 답안 코딩

--- 실전 Verilog 복습 Code/Counter/count_100.v
+++ 실전 Verilog 복습 Code/Counter/count_100.v
@@ -1,16 +1,16 @@
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 
-module counter_100 (
+module count_100 (
     input clk,
     input reset_n,
     output [6:0] o_cnt,
     output [6:0] o_cnt_always
 );
-
 reg [6:0] cnt;
-always @(posedge clk or negedge reset_n) begin // reset_n은 1 -> 0 일때 동작
-    if(!reset_n) begin // !0 = 1 이므로, reset_n == 0 일때 if block 실행
-        cnt <= 0;
+
+always @(posedge clk or negedge reset_n) begin
+    if (!reset_n) begin // reset_n이 0이라면
+        cnt <= 0; // 초기화
     end else if (cnt < 100) begin
         cnt <= cnt + 1;
     end
@@ -18,15 +18,16 @@ end
 assign o_cnt = cnt;
 
 reg [6:0] cnt_always;
+
 always @(posedge clk or negedge reset_n) begin
     if (!reset_n) begin
         cnt_always <= 0;
     end else if (cnt_always == 100 - 1) begin
-        cnt_always <=0; // 99 -> 0
+        cnt_always <= 0;
     end else begin
         cnt_always <= cnt_always + 1;
     end
 end
 assign o_cnt_always = cnt_always;
-    
+
 endmodule
