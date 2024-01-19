ec81a7190735230c83e9ba1d1cf808e40a1ea765
Hanyul <croc0001@naver.com>
2024-01-19 15:28:22 +0900

    2회차 tb 코딩

--- 실전 Verilog 복습 Code/Counter/tb_counter_100.v
+++ 실전 Verilog 복습 Code/Counter/tb_counter_100.v
@@ -1,36 +1,34 @@
-`timescale 1ns/1ps
+// 2회차 tb 코딩
 
-module tb_counter_100;
-reg tb_clk, tb_reset_n;
-wire [6:0] o_cnt;
-wire [6:0] o_cnt_always;
+`timescale 1ns / 1ps
 
-always
-    #5 clk = ~clk;
+module tb_counter_100 ();
+    reg tb_clk; 
+    reg tb_reset_n;
+    reg [6:0] tb_o_cnt; 
+    reg [6:0] tb_o_cnt_always;
 
-initial begin
-    #100
-    $display("initialize at [%d]", $time);
-    tb_reset_n <= 1;
-    tb_clk     <= 0;
+    initial begin
+        $display("initialize at [%d]", $time);
+        tb_clk <= 0;
+        tb_reset_n <= 1;
+        #5
+        tb_reset_n <=0;
+        #5
+        $display("start at [%d]", $time);
+        #200
+        tb_reset_n <=1;
+        #5
+        $display("stop at [%d]", $time);
+    end
 
-    $display("reset at [%d]", $time);
-    #100
-    tb_reset_n <= 0;    
-    #10
-    tb_reset_n <= 1;
-    #10
-    $display("start at [%d]", $time);
-    #2000
-    $display("finish at [%d]", $time);
-    $finish
-end
+    always
+        #5 tb_clk = ~tb_clk;
 
-counter_100 u_counter_100 (
-    .clk (tb_clk),
-    .reset_n (tb_reset_n),
-    .o_cnt (tb_o_cnt),
-    .o_cnt_always (tb_o_cnt_always)
-);
-
+    counter_100 u_counter_100(
+        .clk (tb_clk),
+        .reset_n (tb_reset_n),
+        .o_cnt (tb_o_cnt),
+        .o_cnt_always (tb_o_cnt_always)
+    );
 endmodule
